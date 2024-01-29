6235fc18dc612a5057f50da77a6c06efe2c00652
aloogladys <aloogladys@gmail.com>
2024-01-29 15:23:03 -0500

    created api documentation using postmand and tested api

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include 
 from notes_app.views import sign_in, register , success
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,6 +9,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path('', sign_in),
     path('register/', register),
     path('success/', success),
